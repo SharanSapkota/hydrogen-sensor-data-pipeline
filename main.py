0902232a9e1c5787d7523838624e8cc2e8bd64d5
Sharan Sapkota <sapkotarambbo@gmail.com>
2026-02-25 06:03:07 +0200

    Add a different thread for data transformation to vector

--- main.py
+++ main.py
@@ -1,25 +1,29 @@
-
+import time
+from threading import Thread
 from app.mqtt import MqttClient
+from app import build_sensor_value_vector
 
+TOTAL_NUMBER_OF_SENSORS = 10
+
+sensor_buffer = {}
 def main():
     mqtt_client = MqttClient(broker="localhost", port=1883, topic="test/topic", on_data=handleMQTTData)
     mqtt_client.connect()
 
 def handleMQTTData(data):
-    # Todos:
-    # 1) transform data to [s1, s2,... sn]
-    # 2) pass the transformed data into model
-    # 3) pass the data into the model
-    # 4) After model gives the output
-    # 5) Transform the output into json
-    # 6) publish in the MQTT
-    print(data)
-    # Solution 1)
-    
-    # type of the data will be json()
+    sensorId = data["sensorId"]
+    ppm = data["hydrogenPpm"]
+    sensor_buffer[sensorId] = ppm
 
 
-
+def prediction_loop():
+    transformed_sensor_data = build_sensor_value_vector(sensor_buffer)
+    # Pass this value to the model
+    time.sleep(5)
 
 if __name__ == "__main__":
     main()
+    while (true):
+        worker = Thread(target=prediction_loop)
+        worker.daemon = True
+        worker.start()
